8687741c07850382dbe604b503b5dbd92939d0c2
Krzysztof <krzysztofdabkowski1@gmail.com>
2019-10-29 20:24:31 +0100

    czyste formularze

--- foodUp/urls.py
+++ foodUp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('user_register', views.user_register, name='user_register'),
+    path('company_register', views.company_register, name='company_register')
 ]
