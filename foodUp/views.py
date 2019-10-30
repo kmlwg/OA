1b76c212a538984cbe9b277cefcb0f91cb08c5d4
krzysztofdabkowski1 <33841285+krzysztofdabkowski1@users.noreply.github.com>
2019-10-30 10:59:15 +0100

    Register form (#2)
    * czyste formularze
    
    * usuniecie pustych linii w models.py

--- foodUp/views.py
+++ foodUp/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .forms import UserRegisterForm, CompanyRegisterForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'foodUp/index.html')
+
+def user_register(request):
+	form= UserRegisterForm(request.POST or None)
+	if form.is_valid():
+		form.save(commit=True)
+		form= UserRegisterForm()
+
+	context = {
+		'form': form
+		}
+	return render(request, 'foodUp/user_register.html', context)
+
+
+def company_register(request):
+	form= CompanyRegisterForm(request.POST or None)
+	if form.is_valid():
+		form.save(commit=True)
+		form= CompanyRegisterForm()
+
+	context = {
+		'form': form
+		}
+	return render(request, 'foodUp/company_register.html', context)
