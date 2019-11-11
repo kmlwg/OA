59c89478890ff0643de3e166bb07f144e8990bb8
kmlwg <32034001+kmlwg@users.noreply.github.com>
2019-11-11 21:58:55 +0100

    Login system (#7)
    * whatever
    
    * Logim system alpha

--- foodUp/views.py
+++ foodUp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, REDIRECT_FIELD_NAME
 from django.shortcuts import redirect
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView, RedirectView
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from django.utils.http import is_safe_url
 
 from .forms import UserProfileSignUpForm, CompanyProfileSignUpForm
 from .models import User
@@ -12,7 +18,12 @@ from .models import User
 def index(request):
     return render(request, 'foodUp/index.html')
 
+
+
 class UserProfileSignUpView(CreateView):
+    '''
+    A view for signin up a standard user
+    '''
     model = User
     form_class = UserProfileSignUpForm
     template_name='foodUp/signup_form_user.html'
@@ -31,6 +42,9 @@ class UserProfileSignUpView(CreateView):
 
 
 class CompanyProfileSignUpView(CreateView):
+    '''
+    A view for signing in a company user
+    '''
     model = User
     form_class = CompanyProfileSignUpForm
     template_name='foodUp/signup_form_company.html'
@@ -45,3 +59,6 @@ class CompanyProfileSignUpView(CreateView):
         login(self.request, user)
         # TODO narazie dummy, zmienić na inny widok
         return redirect('index')
+
+
+
