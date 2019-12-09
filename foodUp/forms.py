a7e146c4e9d38002f5df6794facee9b3e250b0bd
Radosław Czapp <czappradoslaw@gmail.com>
2019-12-09 23:06:12 +0100

    New front-end

--- foodUp/forms.py
+++ foodUp/forms.py
@@ -2,34 +2,37 @@ from django import forms
 from .models import Comment, Post
 from users.models import Favourites
 
+
 class Search(forms.Form):
- s = forms.CharField()
- s1 = forms.CharField(required=False)
+    s = forms.CharField()
+    s1 = forms.CharField(required=False)
 
 
 class Category(forms.Form):
-	OPTIONS = (
-		('pizza', 'pizza'),
-		('beer', 'beer'),
-		('pasta', 'pasta'),
-		('burger', 'burger')
-	)
-	s = forms.CharField(required=False)
-	c = forms.MultipleChoiceField(required=False, widget=forms.CheckboxSelectMultiple, choices = OPTIONS)
-	r = forms.DecimalField(required = False)
-	
+    OPTIONS = (
+        ('pizza', 'pizza'),
+        ('beer', 'beer'),
+        ('pasta', 'pasta'),
+        ('burger', 'burger')
+    )
+    search = forms.CharField(required=False)
+    category = forms.MultipleChoiceField(required=False, widget=forms.CheckboxSelectMultiple, choices=OPTIONS)
+    rating = forms.DecimalField(required=False)
+
 
 class MakeComment(forms.ModelForm):
-	class Meta:
-		model = Comment
-		fields =['com']
-		
+    class Meta:
+        model = Comment
+        fields = ['com']
+
+
 class NewPost(forms.ModelForm):
-	class Meta:
-		model = Post
-		fields = ['content']
-		
+    class Meta:
+        model = Post
+        fields = ['content']
+
+
 class SaveFavourite(forms.ModelForm):
-	class Meta:
-		model = Favourites
-		exclude = ['user', 'profile']
+    class Meta:
+        model = Favourites
+        exclude = ['user', 'profile']
